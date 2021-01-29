a1382a2f86cf36a367f4a6e1e90bef2f8764eb2c
Junior <wojunior90@outlook.com>
2021-01-28 21:45:50 -0300

    Criação projeto.

---
+++ AgoraTemqueir/AgoraTemqueir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgoraTemqueir
+//
+//  Created by Wagner Ongaro Junior on 28/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
